776f7adf3fbf9f12f8891d0eafbba565edc7362a
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-04-17 16:56:36 -0500

    files commit

--- js/cal-js.js
+++ js/cal-js.js
@@ -8,17 +8,39 @@
     var leftOperand = document.getElementById("left-operand");
     var centerOperand = document.getElementById("center-operand");
     var rightOperand = document.getElementById("right-operand");
+    var number = document.getElementsByClassName('number');
+    var operator = document.getElementsByClassName('operator');
+    var equals = document.getElementById('equals');
 
-    document.getElementById("nine").addEventListener('click', getNine);
 
-    function getNine () {
-        leftOperand.innerText = 9;
+    // LEFT INPUT
+
+    for (var i = 0; i < number.length; i++) {
+        number[i].addEventListener('click', function () {
+            if (centerOperand.value === '') {
+                leftOperand.value += this.innerHTML;
+                console.log()
+            } else {
+                rightOperand.value += this.innerHTML;
+            }
+        })
     }
 
+    // CENTER INPUT
 
+    for (var i = 0; i < operator.length; i++) {
+        operator[i].addEventListener('click', function () {
+            centerOperand.value = this.innerHTML;
+        })
+    }
 
+    // EQUATION FUNCTIONALITY (switch?)
 
+    function doMath() {
+        rightOperand.value ==
+    }
 
+    equals.addEventListener('click', doMath)
 
 
 

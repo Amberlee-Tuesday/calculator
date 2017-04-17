744f2ec4f88e296fb1479901ea2144aaea62d581
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-04-17 14:29:42 -0500

    files commit

--- js/cal-js.js
+++ js/cal-js.js
@@ -1,3 +1,31 @@
 /**
  * Created by canidmars on 4/17/17.
  */
+
+(function () {
+    "use strict";
+
+    var leftOperand = document.getElementById("left-operand");
+    var centerOperand = document.getElementById("center-operand");
+    var rightOperand = document.getElementById("right-operand");
+
+    document.getElementById("nine").addEventListener('click', getNine);
+
+    function getNine () {
+        leftOperand.innerText = 9;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
